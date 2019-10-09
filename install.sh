64668f3d1eb927167e26fce08174db2e5d2d8503
michael <56309249+michaelrenyu2019@users.noreply.github.com>
2019-10-09 11:06:29 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -220,7 +220,7 @@ ssl_install(){
 
 }
 domain_check(){
-    stty erase '^H' && read -p "请输入你的域名信息(eg:www.wulabing.com):" domain
+    stty erase '^H' && read -p "请输入你的域名信息(eg:www.wulabing.com):" www.michaelry2019.cf
     domain_ip=`ping ${domain} -c 1 | sed '1{s/[^(]*(//;s/).*//;q}'`
     echo -e "${OK} ${GreenBG} 正在获取 公网ip 信息，请耐心等待 ${Font}"
     local_ip=`curl -4 ip.sb`
